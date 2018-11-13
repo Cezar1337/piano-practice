b6983151c5f790c67a1c0c78947c0f5b31e4570b
Cezar1337 <cezary.kiedrowski@gmail.com>
2018-11-13 13:58:04 +0100

    Initial commit

---
+++ Piano Practice.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace piano_practice
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window));
+            this.c_key = new System.Windows.Forms.PictureBox();
+            this.d_key = new System.Windows.Forms.PictureBox();
+            this.e_key = new System.Windows.Forms.PictureBox();
+            this.f_key = new System.Windows.Forms.PictureBox();
+            this.g_key = new System.Windows.Forms.PictureBox();
+            this.a_key = new System.Windows.Forms.PictureBox();
+            this.b_key = new System.Windows.Forms.PictureBox();
+            this.c_sharp_key = new System.Windows.Forms.PictureBox();
+            this.d_sharp_key = new System.Windows.Forms.PictureBox();
+            this.f_sharp_key = new System.Windows.Forms.PictureBox();
+            this.g_sharp_key = new System.Windows.Forms.PictureBox();
+            this.a_sharp_key = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.c_key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d_key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.e_key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.f_key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.g_key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.a_key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b_key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c_sharp_key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d_sharp_key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.f_sharp_key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.g_sharp_key)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.a_sharp_key)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // c_key
+            // 
+            this.c_key.BackColor = System.Drawing.Color.White;
+            this.c_key.Image = ((System.Drawing.Image)(resources.GetObject("c_key.Image")));
+            this.c_key.Location = new System.Drawing.Point(72, 608);
+            this.c_key.Margin = new System.Windows.Forms.Padding(0);
+            this.c_key.MaximumSize = new System.Drawing.Size(160, 360);
+            this.c_key.MinimumSize = new System.Drawing.Size(160, 360);
+            this.c_key.Name = "c_key";
+            this.c_key.Size = new System.Drawing.Size(160, 360);
+            this.c_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.c_key.TabIndex = 0;
+            this.c_key.TabStop = false;
+            // 
+            // d_key
+            // 
+            this.d_key.BackColor = System.Drawing.Color.White;
+            this.d_key.Image = ((System.Drawing.Image)(resources.GetObject("d_key.Image")));
+            this.d_key.Location = new System.Drawing.Point(232, 608);
+            this.d_key.Margin = new System.Windows.Forms.Padding(0);
+            this.d_key.MaximumSize = new System.Drawing.Size(160, 360);
+            this.d_key.MinimumSize = new System.Drawing.Size(160, 360);
+            this.d_key.Name = "d_key";
+            this.d_key.Size = new System.Drawing.Size(160, 360);
+            this.d_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.d_key.TabIndex = 1;
+            this.d_key.TabStop = false;
+            // 
+            // e_key
+            // 
+            this.e_key.BackColor = System.Drawing.Color.White;
+            this.e_key.Image = ((System.Drawing.Image)(resources.GetObject("e_key.Image")));
+            this.e_key.Location = new System.Drawing.Point(392, 608);
+            this.e_key.Margin = new System.Windows.Forms.Padding(0);
+            this.e_key.MaximumSize = new System.Drawing.Size(160, 360);
+            this.e_key.MinimumSize = new System.Drawing.Size(160, 360);
+            this.e_key.Name = "e_key";
+            this.e_key.Size = new System.Drawing.Size(160, 360);
+            this.e_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.e_key.TabIndex = 2;
+            this.e_key.TabStop = false;
+            // 
+            // f_key
+            // 
+            this.f_key.BackColor = System.Drawing.Color.White;
+            this.f_key.Image = ((System.Drawing.Image)(resources.GetObject("f_key.Image")));
+            this.f_key.Location = new System.Drawing.Point(552, 608);
+            this.f_key.Margin = new System.Windows.Forms.Padding(0);
+            this.f_key.MaximumSize = new System.Drawing.Size(160, 360);
+            this.f_key.MinimumSize = new System.Drawing.Size(160, 360);
+            this.f_key.Name = "f_key";
+            this.f_key.Size = new System.Drawing.Size(160, 360);
+            this.f_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.f_key.TabIndex = 3;
+            this.f_key.TabStop = false;
+            // 
+            // g_key
+            // 
+            this.g_key.BackColor = System.Drawing.Color.White;
+            this.g_key.Image = ((System.Drawing.Image)(resources.GetObject("g_key.Image")));
+            this.g_key.Location = new System.Drawing.Point(712, 608);
+            this.g_key.Margin = new System.Windows.Forms.Padding(0);
+            this.g_key.MaximumSize = new System.Drawing.Size(160, 360);
+            this.g_key.MinimumSize = new System.Drawing.Size(160, 360);
+            this.g_key.Name = "g_key";
+            this.g_key.Size = new System.Drawing.Size(160, 360);
+            this.g_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.g_key.TabIndex = 4;
+            this.g_key.TabStop = false;
+            // 
+            // a_key
+            // 
+            this.a_key.BackColor = System.Drawing.Color.White;
+            this.a_key.Image = ((System.Drawing.Image)(resources.GetObject("a_key.Image")));
+            this.a_key.Location = new System.Drawing.Point(872, 608);
+            this.a_key.Margin = new System.Windows.Forms.Padding(0);
+            this.a_key.MaximumSize = new System.Drawing.Size(160, 360);
+            this.a_key.MinimumSize = new System.Drawing.Size(160, 360);
+            this.a_key.Name = "a_key";
+            this.a_key.Size = new System.Drawing.Size(160, 360);
+            this.a_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.a_key.TabIndex = 5;
+            this.a_key.TabStop = false;
+            // 
+            // b_key
+            // 
+            this.b_key.BackColor = System.Drawing.Color.White;
+            this.b_key.Image = ((System.Drawing.Image)(resources.GetObject("b_key.Image")));
+            this.b_key.Location = new System.Drawing.Point(1032, 608);
+            this.b_key.Margin = new System.Windows.Forms.Padding(0);
+            this.b_key.MaximumSize = new System.Drawing.Size(160, 360);
+            this.b_key.MinimumSize = new System.Drawing.Size(160, 360);
+            this.b_key.Name = "b_key";
+            this.b_key.Size = new System.Drawing.Size(160, 360);
+            this.b_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.b_key.TabIndex = 6;
+            this.b_key.TabStop = false;
+            // 
+            // c_sharp_key
+            // 
+            this.c_sharp_key.BackColor = System.Drawing.Color.Black;
+            this.c_sharp_key.Image = ((System.Drawing.Image)(resources.GetObject("c_sharp_key.Image")));
+            this.c_sharp_key.Location = new System.Drawing.Point(198, 608);
+            this.c_sharp_key.Margin = new System.Windows.Forms.Padding(0);
+            this.c_sharp_key.MaximumSize = new System.Drawing.Size(70, 220);
+            this.c_sharp_key.MinimumSize = new System.Drawing.Size(70, 220);
+            this.c_sharp_key.Name = "c_sharp_key";
+            this.c_sharp_key.Size = new System.Drawing.Size(70, 220);
+            this.c_sharp_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.c_sharp_key.TabIndex = 7;
+            this.c_sharp_key.TabStop = false;
+            // 
+            // d_sharp_key
+            // 
+            this.d_sharp_key.BackColor = System.Drawing.Color.Black;
+            this.d_sharp_key.Image = ((System.Drawing.Image)(resources.GetObject("d_sharp_key.Image")));
+            this.d_sharp_key.Location = new System.Drawing.Point(357, 608);
+            this.d_sharp_key.Margin = new System.Windows.Forms.Padding(0);
+            this.d_sharp_key.MaximumSize = new System.Drawing.Size(70, 220);
+            this.d_sharp_key.MinimumSize = new System.Drawing.Size(70, 220);
+            this.d_sharp_key.Name = "d_sharp_key";
+            this.d_sharp_key.Size = new System.Drawing.Size(70, 220);
+            this.d_sharp_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.d_sharp_key.TabIndex = 8;
+            this.d_sharp_key.TabStop = false;
+            // 
+            // f_sharp_key
+            // 
+            this.f_sharp_key.BackColor = System.Drawing.Color.Black;
+            this.f_sharp_key.Image = ((System.Drawing.Image)(resources.GetObject("f_sharp_key.Image")));
+            this.f_sharp_key.Location = new System.Drawing.Point(684, 608);
+            this.f_sharp_key.Margin = new System.Windows.Forms.Padding(0);
+            this.f_sharp_key.MaximumSize = new System.Drawing.Size(70, 220);
+            this.f_sharp_key.MinimumSize = new System.Drawing.Size(70, 220);
+            this.f_sharp_key.Name = "f_sharp_key";
+            this.f_sharp_key.Size = new System.Drawing.Size(70, 220);
+            this.f_sharp_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.f_sharp_key.TabIndex = 9;
+            this.f_sharp_key.TabStop = false;
+            // 
+            // g_sharp_key
+            // 
+            this.g_sharp_key.BackColor = System.Drawing.Color.Black;
+            this.g_sharp_key.Image = ((System.Drawing.Image)(resources.GetObject("g_sharp_key.Image")));
+            this.g_sharp_key.Location = new System.Drawing.Point(838, 608);
+            this.g_sharp_key.Margin = new System.Windows.Forms.Padding(0);
+            this.g_sharp_key.MaximumSize = new System.Drawing.Size(70, 220);
+            this.g_sharp_key.MinimumSize = new System.Drawing.Size(70, 220);
+            this.g_sharp_key.Name = "g_sharp_key";
+            this.g_sharp_key.Size = new System.Drawing.Size(70, 220);
+            this.g_sharp_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.g_sharp_key.TabIndex = 10;
+            this.g_sharp_key.TabStop = false;
+            // 
+            // a_sharp_key
+            // 
+            this.a_sharp_key.BackColor = System.Drawing.Color.Black;
+            this.a_sharp_key.Image = ((System.Drawing.Image)(resources.GetObject("a_sharp_key.Image")));
+            this.a_sharp_key.Location = new System.Drawing.Point(992, 608);
+            this.a_sharp_key.Margin = new System.Windows.Forms.Padding(0);
+            this.a_sharp_key.MaximumSize = new System.Drawing.Size(70, 220);
+            this.a_sharp_key.MinimumSize = new System.Drawing.Size(70, 220);
+            this.a_sharp_key.Name = "a_sharp_key";
+            this.a_sharp_key.Size = new System.Drawing.Size(70, 220);
+            this.a_sharp_key.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.a_sharp_key.TabIndex = 11;
+            this.a_sharp_key.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 977);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.a_sharp_key);
+            this.Controls.Add(this.g_sharp_key);
+            this.Controls.Add(this.f_sharp_key);
+            this.Controls.Add(this.d_sharp_key);
+            this.Controls.Add(this.c_sharp_key);
+            this.Controls.Add(this.b_key);
+            this.Controls.Add(this.a_key);
+            this.Controls.Add(this.g_key);
+            this.Controls.Add(this.f_key);
+            this.Controls.Add(this.e_key);
+            this.Controls.Add(this.d_key);
+            this.Controls.Add(this.c_key);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1280, 1024);
+            this.Name = "Window";
+            this.Text = "Window";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Window_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Window_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.c_key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d_key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.e_key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.f_key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.g_key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.a_key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.b_key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.c_sharp_key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.d_sharp_key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.f_sharp_key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.g_sharp_key)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.a_sharp_key)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox c_key;
+        private System.Windows.Forms.PictureBox d_key;
+        private System.Windows.Forms.PictureBox e_key;
+        private System.Windows.Forms.PictureBox f_key;
+        private System.Windows.Forms.PictureBox g_key;
+        private System.Windows.Forms.PictureBox a_key;
+        private System.Windows.Forms.PictureBox b_key;
+        private System.Windows.Forms.PictureBox c_sharp_key;
+        private System.Windows.Forms.PictureBox d_sharp_key;
+        private System.Windows.Forms.PictureBox f_sharp_key;
+        private System.Windows.Forms.PictureBox g_sharp_key;
+        private System.Windows.Forms.PictureBox a_sharp_key;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Piano Practice.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace piano_practice
+{
+    public partial class Window : Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            System.Media.SoundPlayer key = new System.Media.SoundPlayer();
+            switch (e.KeyCode.ToString())
+            {
+                case "Z":
+                    c_key.BackColor=Color.Aqua;
+                    key.SoundLocation = @"sound\C.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+                case "X":
+                    d_key.BackColor = Color.Aqua;
+                    key.SoundLocation = @"sound\D.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+                case "C":
+                    e_key.BackColor = Color.Aqua;
+                    key.SoundLocation = @"sound\E.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+                case "V":
+                    f_key.BackColor = Color.Aqua;
+                    key.SoundLocation = @"sound\F.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+                case "B":
+                    g_key.BackColor = Color.Aqua;
+                    key.SoundLocation = @"sound\G.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+                case "N":
+                    a_key.BackColor = Color.Aqua;
+                    key.SoundLocation = @"sound\A.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+                case "M":
+                    b_key.BackColor = Color.Aqua;
+                    key.SoundLocation = @"sound\B.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+                case "S":
+                    c_sharp_key.BackColor = Color.DarkBlue;
+                    key.SoundLocation = @"sound\C#.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+                case "D":
+                    d_sharp_key.BackColor = Color.DarkBlue;
+                    key.SoundLocation = @"sound\D#.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+                case "G":
+                    f_sharp_key.BackColor = Color.DarkBlue;
+                    key.SoundLocation = @"sound\F#.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+                case "H":
+                    g_sharp_key.BackColor = Color.DarkBlue;
+                    key.SoundLocation = @"sound\G#.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+                case "J":
+                    a_sharp_key.BackColor = Color.DarkBlue;
+                    key.SoundLocation = @"sound\A#.wav";
+                    key.Load();
+                    key.Play();
+                    break;
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "Z":
+                    c_key.BackColor = Color.White;
+                    break;
+                case "X":
+                    d_key.BackColor = Color.White;
+                    break;
+                case "C":
+                    e_key.BackColor = Color.White;
+                    break;
+                case "V":
+                    f_key.BackColor = Color.White;
+                    break;
+                case "B":
+                    g_key.BackColor = Color.White;
+                    break;
+                case "N":
+                    a_key.BackColor = Color.White;
+                    break;
+                case "M":
+                    b_key.BackColor = Color.White;
+                    break;
+                case "S":
+                    c_sharp_key.BackColor = Color.Black;
+                    break;
+                case "D":
+                    d_sharp_key.BackColor = Color.Black;
+                    break;
+                case "G":
+                    f_sharp_key.BackColor = Color.Black;
+                    break;
+                case "H":
+                    g_sharp_key.BackColor = Color.Black;
+                    break;
+                case "J":
+                    a_sharp_key.BackColor = Color.Black;
+                    break;
+            }
+        }
+    }
+}

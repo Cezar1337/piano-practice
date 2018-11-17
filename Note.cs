c5d5b304c93ef725a93f54095144b9d8d442a2ca
Cezar1337 <cezary.kiedrowski@gmail.com>
2018-11-17 14:53:49 +0100

    Note class, multiple sounds
    Added a Note class, that translates and hold information about a note
    Used System.Windows.Media.MediaPlayer() in order to play multiple sounds

---
+++ Note.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace piano_practice
+{
+    public class Note
+    {
+        private Color highlight_color;
+        private Color original_color;
+        private string note_code;
+        public bool playing = false;
+
+        public Note()
+        {
+            this.highlight_color = Color.Aqua;
+            this.note_code = "";
+            this.original_color = Color.White;
+            this.playing = false;
+        }
+
+        public void Translate(string code)
+        {
+            switch(code)
+            {
+                case "Z":
+                    this.note_code = "C";
+                    break;
+                case "X":
+                    this.note_code = "D";
+                    break;
+                case "C":
+                    this.note_code = "E";
+                    break;
+                case "V":
+                    this.note_code = "F";
+                    break;
+                case "B":
+                    this.note_code = "G";
+                    break;
+                case "N":
+                    this.note_code = "A";
+                    break;
+                case "M":
+                    this.note_code = "B";
+                    break;
+                case "S":
+                    this.note_code = "C#";
+                    break;
+                case "D":
+                    this.note_code = "D#";
+                    break;
+                case "G":
+                    this.note_code = "F#";
+                    break;
+                case "H":
+                    this.note_code = "G#";
+                    break;
+                case "J":
+                    this.note_code = "A#";
+                    break;
+            }
+        }
+
+        public void Play()
+        {
+            var key = new System.Windows.Media.MediaPlayer();
+            if (!playing)
+            {
+                playing = true;
+                key.Open(new System.Uri(@"sound\" + this.note_code + ".wav", UriKind.Relative));
+                key.Play();
+            }
+            if (playing)
+            {
+                playing = false;
+            }
+
+        }
+    }
+}
--- Piano Practice.cs
+++ Piano Practice.cs
@@ -12,9 +12,28 @@ namespace piano_practice
 {
     public partial class Window : Form
     {
+        Note[] notes = new Note[12];
         public Window()
         {
             InitializeComponent();
+
+            for(int i=0;i<12;i++)
+            {
+                notes[i] = new Note();
+            }
+
+            notes[0].Translate("Z");
+            notes[1].Translate("X");
+            notes[2].Translate("C");
+            notes[3].Translate("V");
+            notes[4].Translate("B");
+            notes[5].Translate("N");
+            notes[6].Translate("M");
+            notes[7].Translate("S");
+            notes[8].Translate("D");
+            notes[9].Translate("G");
+            notes[10].Translate("H");
+            notes[11].Translate("J");
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -24,75 +43,51 @@ namespace piano_practice
             {
                 case "Z":
                     c_key.BackColor=Color.Aqua;
-                    key.SoundLocation = @"sound\C.wav";
-                    key.Load();
-                    key.Play();
+                    notes[0].Play();
                     break;
                 case "X":
                     d_key.BackColor = Color.Aqua;
-                    key.SoundLocation = @"sound\D.wav";
-                    key.Load();
-                    key.Play();
+                    notes[1].Play();
                     break;
                 case "C":
                     e_key.BackColor = Color.Aqua;
-                    key.SoundLocation = @"sound\E.wav";
-                    key.Load();
-                    key.Play();
+                    notes[2].Play();
                     break;
                 case "V":
                     f_key.BackColor = Color.Aqua;
-                    key.SoundLocation = @"sound\F.wav";
-                    key.Load();
-                    key.Play();
+                    notes[3].Play();
                     break;
                 case "B":
                     g_key.BackColor = Color.Aqua;
-                    key.SoundLocation = @"sound\G.wav";
-                    key.Load();
-                    key.Play();
+                    notes[4].Play();
                     break;
                 case "N":
                     a_key.BackColor = Color.Aqua;
-                    key.SoundLocation = @"sound\A.wav";
-                    key.Load();
-                    key.Play();
+                    notes[5].Play();
                     break;
                 case "M":
                     b_key.BackColor = Color.Aqua;
-                    key.SoundLocation = @"sound\B.wav";
-                    key.Load();
-                    key.Play();
+                    notes[6].Play();
                     break;
                 case "S":
                     c_sharp_key.BackColor = Color.DarkBlue;
-                    key.SoundLocation = @"sound\C#.wav";
-                    key.Load();
-                    key.Play();
+                    notes[7].Play();
                     break;
                 case "D":
                     d_sharp_key.BackColor = Color.DarkBlue;
-                    key.SoundLocation = @"sound\D#.wav";
-                    key.Load();
-                    key.Play();
+                    notes[8].Play();
                     break;
                 case "G":
                     f_sharp_key.BackColor = Color.DarkBlue;
-                    key.SoundLocation = @"sound\F#.wav";
-                    key.Load();
-                    key.Play();
+                    notes[9].Play();
                     break;
                 case "H":
                     g_sharp_key.BackColor = Color.DarkBlue;
-                    key.SoundLocation = @"sound\G#.wav";
-                    key.Load();
-                    key.Play();
+                    notes[10].Play();
                     break;
                 case "J":
                     a_sharp_key.BackColor = Color.DarkBlue;
-                    key.SoundLocation = @"sound\A#.wav";
-                    key.Load();
-                    key.Play();
+                    notes[11].Play();
                     break;
             }
         }
